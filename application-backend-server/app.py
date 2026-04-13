c6f3fc925211a53d2792bb7b0b9711de16e75dd6
KhangNguyen <khangkhangng200528@gmail.com>
2026-04-13 17:05:38 +0700

    thanh vien B task done

--- application-backend-server/app.py
+++ application-backend-server/app.py
@@ -1,9 +1,16 @@
+import json
 from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
     return jsonify(message="Hello from App Server!")
+
+@app.route("/student")
+def student():
+    with open("students.json") as f:
+        data = json.load(f)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
--- relational-database-server/init/init.sql
+++ relational-database-server/init/init.sql
@@ -1,9 +1,15 @@
-CREATE DATABASE IF NOT EXISTS minicloud;
-USE minicloud;
+CREATE DATABASE IF NOT EXISTS studentdb;
+USE studentdb;
 
-CREATE TABLE notes (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100)
+CREATE TABLE students (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  student_id VARCHAR(10),
+  fullname VARCHAR(100),
+  dob DATE,
+  major VARCHAR(50)
 );
 
-INSERT INTO notes(title) VALUES ("Hello from MariaDB!");
+INSERT INTO students (student_id, fullname, dob, major) VALUES
+  ('SV001', 'Nguyen Van An', '2003-05-15', 'Computer Science'),
+  ('SV002', 'Tran Thi Bich', '2003-08-22', 'Information Technology'),
+  ('SV003', 'Le Hoang Cuong', '2002-12-01', 'Software Engineering');
